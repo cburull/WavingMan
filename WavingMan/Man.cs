9fdf90f62e9e4ef3321733e7877ccd336f0262ce
cburull <88709887+cburull@users.noreply.github.com>
2022-06-28 12:52:09 +0200

    Add project files.

---
+++ WavingMan/Man.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace WavingMan
+{
+    public class Man
+    {
+        public Position Position { get; }
+        public Position Speed { get; }
+
+        internal bool _shouldWaveNextTime = false;
+
+        public Man(int x, int y, int dx, int dy)
+        {
+
+            Position = new Position(x, y);
+            Speed = new Position(dx, dy);
+        }
+
+        public virtual void Show()
+        {
+            SetCursorTop();
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? " o/" : " o");
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? "/|"  : " |");
+            SetCursorLeft();
+            Console.Write("/ \\");
+            _shouldWaveNextTime = !_shouldWaveNextTime;
+        }
+
+        internal void SetCursorTop()
+        {
+            Console.CursorTop = Math.Max(Position.Y, 0);
+        }
+
+        internal void SetCursorLeft()
+        {
+            Console.CursorLeft = Math.Max(Position.X - 1, 0);
+        }
+
+        public void Move()
+        {
+            var windowWidth = Console.WindowWidth;
+            var windowHeight = Console.WindowHeight;
+
+            if (Position.X <= 1) Speed.X = 1;
+            if (Position.X >= windowWidth - 1) Speed.X = -1;
+            if (Position.Y <= 1) Speed.Y = 1;
+            if (Position.Y >= windowHeight - 1) Speed.Y = -1;
+
+            Position.Move(Speed);
+        }
+    }
+}
